f832d868a89c788335d0a1b3a5a455d338a2a080
samlarson94 <samlarson94@gmail.com>
2022-08-30 13:13:21 -0500

    Adding and testing dashboard routes.

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from app.routes import home
+from app.routes import home, dashboard
 # Can import home directly bc routes init file imported and renamed the object
 from flask import Flask
 
@@ -15,5 +15,6 @@ def create_app(test_config=None):
 
   # register routes
   app.register_blueprint(home)
+  app.register_blueprint(dashboard)
 
   return app
