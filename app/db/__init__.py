317969ef26aea80e5e23c89ae69c18f7b451cbf6
samlarson94 <samlarson94@gmail.com>
2022-09-01 17:19:23 -0500

    Adding in db connection to be used on live (non-local) site.

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -18,6 +18,7 @@ Base = declarative_base()
 
 def init_db():
   Base.metadata.create_all(engine)
+  app.teardown_appcontext(close_db)
 
 # Set up function to return new session-connection object
     #Utilizing "g" object  to save the current connection and avoid repeat connections
@@ -26,3 +27,13 @@ def get_db():
     # store db connection in app context
     g.db = Session()
   return g.db
+
+# Set up connection to database that will work on Heroku & other hosting sites
+def close_db(e=None):
+  db = g.pop('db', None)
+  # pop method attempts to find and remove db from the g object.
+    # if db exists, then db.close will end the connection
+    # The app.teardown_appcontext(close_db), in init_db(), flask method will automate db.close
+
+  if db is not None:
+    db.close()
