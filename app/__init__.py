d0b3adc72c7ad45d2dd29276ef19496c8c7e7c26
samlarson94 <samlarson94@gmail.com>
2022-09-01 17:06:51 -0500

    Implementing application context in flash.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from app.routes import home, dashboard
 # Can import home directly bc routes init file imported and renamed the object
 from flask import Flask
+from app.db import init_db
 
 def create_app(test_config=None):
   # set up app config
@@ -17,4 +18,5 @@ def create_app(test_config=None):
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
 
+  init_db()
   return app
